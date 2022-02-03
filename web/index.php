3850433586c118b31a7b3a62d91f1dd2ef6b4336
Jan Willem Kolkman <jwkolkman@gmail.com>
2022-02-03 17:04:36 +0100

    Updated readme and web files

--- web/index.php
+++ web/index.php
@@ -1,5 +1,5 @@
 <?php
-require_once(__DIR__ . '/../config.php');
+require_once(__DIR__ . '/../config/netlipress.php');
 require_once(__DIR__ . '/../vendor/autoload.php');
 $app = new Netlipress\Application();
 $app->run();
