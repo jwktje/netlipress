7b8dbea33cf5a257e06a57157f3fe22e8bba4323
Jan Willem Kolkman <jwkolkman@gmail.com>
2022-02-04 21:50:06 +0100

    Some cleanup and documentation.

--- web/theme/header.php
+++ web/theme/header.php
@@ -9,4 +9,4 @@
 
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
-<?php get_template_part('template-parts/partials/navbar');?>
+<?php get_template_part('template-parts/partials/navigation');?>
---
+++ web/theme/template-parts/partials/navigation.php
@@ -0,0 +1,5 @@
+<nav class="main-navigation">
+    <div class="menu-wrapper">
+        <?php wp_nav_menu(['theme_location' => 'primary', 'menu_id' => 'primary-menu']); ?>
+    </div>
+</nav>
