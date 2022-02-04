bd9223fccba94b710aadaacdbaa903d4dcfbb17f
Jan Willem Kolkman <jwkolkman@gmail.com>
2022-02-04 20:50:28 +0100

    Updated backend config, readme and default debug config

--- config/netlipress.php
+++ config/netlipress.php
@@ -1,5 +1,5 @@
 <?php
 define('APP_ROOT', realpath(__DIR__ . '/../'));
-define('DEBUG', true);
+define('DEBUG', !defined('NETLIFY')); //Disable debug on deployment
 define('SITE_NAME', 'Netlipress');
 define('USE_MIX', true);
