f3aab4954451fce3821b21ba0af72f27aa08a92a
Jan Willem Kolkman <jwkolkman@gmail.com>
2022-02-05 15:42:25 +0100

    Updated env check

--- config/netlipress.php
+++ config/netlipress.php
@@ -1,5 +1,5 @@
 <?php
 define('APP_ROOT', realpath(__DIR__ . '/../'));
-define('DEBUG', !defined('NETLIFY')); //Disable debug on deployment
+define('DEBUG', !getenv('NETLIFY')); //Disable debug on deployment
 define('SITE_NAME', 'Netlipress');
 define('USE_MIX', true);
